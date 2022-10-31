1db68eb4751a833a31a5a2a7595708f66eb2cf8a
airRnot1106 <yngw.rintaro@gmail.com>
2022-10-31 17:21:33 +0900

    aliasの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~~': `${__dirname}/`,
+      '~~/': `${__dirname}/`,
+      '@@': `${__dirname}/`,
+      '@@/': `${__dirname}/`,
+      '~': `${__dirname}/src/`,
+      '~/': `${__dirname}/src/`,
+      '@': `${__dirname}/src/`,
+      '@/': `${__dirname}/src/`,
+      assets: `${__dirname}/src/assets/`,
+      'assets/': `${__dirname}/src/assets/`,
+    },
+  },
+});
