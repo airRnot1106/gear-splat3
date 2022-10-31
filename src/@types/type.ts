233a1782578549474f1f7ac682f754a4e54ece72
airRnot1106 <yngw.rintaro@gmail.com>
2022-10-31 16:25:33 +0900

    型定義とutilを追加

---
+++ src/@types/type.ts
@@ -0,0 +1,69 @@
+export type SubGearPowerName =
+  | 'ink_recovery_up'
+  | 'ink_resistance_up'
+  | 'ink_saver_main'
+  | 'ink_saver_sub'
+  | 'intensify_action'
+  | 'quick_respawn'
+  | 'quick_super_jump'
+  | 'run_speed_up'
+  | 'special_charge_up'
+  | 'special_power_up'
+  | 'special_saver'
+  | 'sub_power_up'
+  | 'sub_resistance_up'
+  | 'swim_speed_up';
+
+export type HeadGearPowerName =
+  | SubGearPowerName
+  | 'comeback'
+  | 'last_ditch_effort'
+  | 'opening_gambit'
+  | 'tenacity';
+
+export type ClothingGearPowerName =
+  | SubGearPowerName
+  | 'ability_doubler'
+  | 'haunt'
+  | 'ninja_squid'
+  | 'respawn_punisher'
+  | 'thermal_ink';
+
+export type ShoesGearPowerName =
+  | SubGearPowerName
+  | 'drop_roller'
+  | 'object_shredder'
+  | 'stealth_jump';
+
+export type MainGearPowerName =
+  | HeadGearPowerName
+  | ClothingGearPowerName
+  | ShoesGearPowerName;
+
+type GearPowerIconName<T extends MainGearPowerName> = `icon_${T}`;
+
+export type GearPowerRecord<T extends MainGearPowerName | SubGearPowerName> = {
+  gearPower: T;
+  icon: GearPowerIconName<T>;
+};
+
+export interface IGear {
+  id: string;
+  gearPowers: [
+    GearPowerRecord<MainGearPowerName>,
+    GearPowerRecord<SubGearPowerName>,
+    GearPowerRecord<SubGearPowerName>,
+    GearPowerRecord<SubGearPowerName>
+  ];
+}
+
+export interface IGears {
+  id: string;
+  head: IGear;
+  clothing: IGear;
+  shoes: IGear;
+}
+
+export type GearSlotNumber = 0 | 1 | 2 | 3;
+
+export type GearType = 'HEAD' | 'CLOTHING' | 'SHOES';
