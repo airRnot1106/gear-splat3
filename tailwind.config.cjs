45772e560815e50f8b91b1f7fb9a442579403340
airRnot1106 <yngw.rintaro@gmail.com>
2022-11-01 23:07:43 +0900

    テーマを追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,28 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-black': '#333333',
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light'],
+    themes: [
+      {
+        mytheme: {
+          primary: '#E0A82E',
+          secondary: '#F9D72F',
+          accent: '#18182F',
+          neutral: '#3D4451',
+          'base-100': '#FFFFFF',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
   },
 };
