dba6449340fb838ed73fe29043504e616302eab9
airRnot1106 <yngw.rintaro@gmail.com>
2022-11-01 23:13:16 +0900

    h-5%からh-5vhに変更

--- src/components/globals/AppHeader.vue
+++ src/components/globals/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-[5%] items-center border-b-2 bg-gray-100 px-2">
+  <div class="flex h-[5vh] items-center border-b-2 bg-gray-100 px-2">
     <h1 class="select-none text-2xl font-bold">Gear-Splat3</h1>
   </div>
 </template>
