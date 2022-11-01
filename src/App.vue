cca9d00b2ed3b35c68b9fb5eed6b6eb6686a53cf
airRnot1106 <yngw.rintaro@gmail.com>
2022-11-01 23:14:52 +0900

    indexを追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   import AppHeader from '@/components/globals/AppHeader.vue';
+  import index from '@/components/pages/index.vue';
 </script>
 
 <template>
   <div id="app">
     <AppHeader />
+    <index />
   </div>
 </template>
